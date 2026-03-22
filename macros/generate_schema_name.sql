c4bef0feb4ed2681a9beca8b045dfc00aa278911
kara <kara_himanshu@yahoo.com>
2026-03-22 19:15:19 +0530

    feat: add dev/prod targets and update generate_schema_name macro
    - dev target writes to DEV_KARAWORK_* schemas
    - prod target writes to clean STAGING/INTERMEDIATE/MARTS schemas
    - updated generate_schema_name macro to handle both environments
    - fixed accepted_values test syntax for dbt 1.11

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,7 +1,19 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
-    {%- if custom_schema_name is none -%}
-        {{ target.schema }}
+
+    {%- if target.name == 'prod' -%}
+        {%- if custom_schema_name is none -%}
+            {{ target.schema }}
+        {%- else -%}
+            {{ custom_schema_name | upper }}
+        {%- endif -%}
+
     {%- else -%}
-        {{ custom_schema_name | upper }}
+        {%- if custom_schema_name is none -%}
+            {{ target.schema | upper }}
+        {%- else -%}
+            {{ target.schema | upper }}_{{ custom_schema_name | upper }}
+        {%- endif -%}
+
     {%- endif -%}
+
 {%- endmacro %}
